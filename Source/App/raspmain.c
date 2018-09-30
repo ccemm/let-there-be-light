56c2423bd7c32b6e6f214b0646ce91efd0028cce
ccemm <mesutcemakin@gmail.com>
2018-09-30 18:43:33 +0300

    - A Button read Imlementation

--- Source/App/raspmain.c
+++ Source/App/raspmain.c
@@ -3,10 +3,34 @@
 #include "pigpio.h"
 #include "time.h"
 
+// ---- Button Pin 17
+// ---- Led Pin 18
+
+void keyStateChanged(int event, int level, uint32_t tick)
+{
+	static bool ledState=false;
+	static int prevTick = 0;
+	
+	if(event = 17 && level == 0)
+	{
+		if(ledState)
+		{
+			ledState =0;
+			gpioWrite(18,0);
+		}
+		else 
+		{
+			ledState =1;
+			gpioWrite(18,1);
+		}
+	}
+}
+
 int main(void)
 {
 	printf("!!! RPI Program Started !!!\n");
 	
+	// init pigpio
 	if(gpioInitialise() <0)
 	{
 		printf("Init Failed \n");
@@ -14,22 +38,26 @@ int main(void)
 		return -1;
 	}
 	
-	if(gpioSetMode(18, PI_OUTPUT) < 0)
+	// Set Pin Directions
+	if(gpioSetMode(18, PI_OUTPUT) < 0 || 
+		gpioSetMode(17, PI_INPUT)<0 )
 	{
 		printf("Set Mod Failed \n");
-		//gpioSetPullUpDown(18, PI_PUD_DOWN); // Sets a pull-down
 		gpioTerminate();
 		return -1;
 	}
-	int val=1;
 	
-	for(int i =0; i<50; i++)
+	// Activate Pull-Up for Button
+	gpioSetPullUpDown(17, PI_PUD_UP); // Sets a pull-down
+	// Glitch Filter 100 Ms
+	gpioGlitchFilter(17, 100000 /*micoseconds == 100Ms */);
+	// Set Call Back For Button Event
+	gpioSetAlertFunc(17, keyStateChanged);
+		
+	while(1)
 	{
 		sleep(1);
-		gpioWrite(18, val);
-		val = (!val);
-	} 
-	
+	}
 	
 	gpioTerminate();
 	return 0;
